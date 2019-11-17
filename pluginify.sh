cd24cce7a5d2bedbdce783014d3d8c238569de05
Mark Custer <fordmadox@gmail.com>
2019-11-16 21:26:16 -0500

    testing fix for bsd

--- pluginify.sh
+++ pluginify.sh
@@ -9,15 +9,15 @@ cat plugins/*/frontend/locales/enums/en.yml \
   >> frontend/locales/enums/en.yml 2>/dev/null
 
 # remove empty lines
-sed -i '/^[[:space:]]*$/d' frontend/locales/en.yml
-sed -i '/^[[:space:]]*$/d' frontend/locales/enums/en.yml
+sed -i '' '/^[[:space:]]*$/d' frontend/locales/en.yml
+sed -i '' '/^[[:space:]]*$/d' frontend/locales/enums/en.yml
 
 # remove all "en:" occurences
-sed -i '/^en:$/d' frontend/locales/en.yml
-sed -i '/^en:$/d' frontend/locales/enums/en.yml
+sed -i '' '/^en:$/d' frontend/locales/en.yml
+sed -i '' '/^en:$/d' frontend/locales/enums/en.yml
 
 # prepend "en:"
-sed -i '1s/^/en:\n/' frontend/locales/en.yml
-sed -i '1s/^/en:\n/' frontend/locales/enums/en.yml
+sed -i '' '1s/^/en:\'$'\n/' frontend/locales/en.yml
+sed -i '' '1s/^/en:\'$'\n/' frontend/locales/enums/en.yml
 
 echo "Updated frontend locales!"
